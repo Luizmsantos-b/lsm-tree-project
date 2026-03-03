1b18259f2020756d0646ce50b697817170435541
Luiz <luiz.marcelo.rabelo.borges.da.costa.santos@ccc.ufcg.edu.br>
2026-03-03 20:24:00 -0300

    Adicionando DockerFiles e o docker-compose

---
+++ docker/Dockerfile.java
@@ -0,0 +1,5 @@
+﻿FROM eclipse-temurin:21-jdk-alpine
+WORKDIR /app
+COPY src/ /app/src/
+RUN mkdir -p /app/out
+CMD ["sh", "-c", "find /app/src -name '*.java' > sources.txt && javac -d /app/out @sources.txt && java -Xmxm -cp /app/out lsm.benchmark.BenchmarkRunner"]
